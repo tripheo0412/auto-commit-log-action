2ed915056119c14f10567f101aa1a45bd9d19e5f
SexyBeast0412 <tripheo0412@gmail.com>
2020-04-22 02:17:57 +0700

    update various stuff

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,21 @@
 import * as core from '@actions/core'
-import github from '@actions/github'
-
 import {spawn} from 'child_process'
 import path from 'path'
-const context = github.context
 const exec = async (cmd: string, args: string[]): Promise<String> =>
   new Promise((resolve, reject) => {
-    core.debug(`Started: ${cmd} ${args.join(' ')}`)
-    core.debug(context.toString())
-    const app = spawn(cmd, args, {stdio: 'inherit'})
-    app.on('close', async code => {
-      if (code !== 0) {
-        const err = new Error(`Invalid status code: ${code}`)
-        return reject(err)
-      }
-      return resolve(code.toString())
-    })
-    app.on('error', reject)
+    try {
+      core.debug(`Started: ${cmd} ${args.join(' ')}`)
+      const app = spawn(cmd, args, {stdio: 'inherit'})
+      app.on('close', async code => {
+        if (code !== 0) {
+          const err = new Error(`Invalid status code: ${code}`)
+          return reject(err)
+        }
+        return resolve(code.toString())
+      })
+    } catch (error) {
+      core.setFailed(error.message)
+    }
   })
 
 const main = async (): Promise<void> => {
